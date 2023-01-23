9b9b85ef44d4fae31c5f51ce430a93164a4f9998
prometeodie <franco.d.r1992@gmail.com>
2023-01-23 20:54:02 -0300

    Portfolio section done

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+// Skills Data
 const skillIntellect = [{
     skillName:'Superhuman Intelligence',
     percent: 100
@@ -32,7 +33,7 @@ const skillPhysicalFitness = [{
     skillName:'Robot Engineering',
     percent: 95
 },{
-    skillName:'Near Superhuman Strength & Agility',
+    skillName:'Strength & Agility',
     percent: 85
 },{
     skillName:'High Alcohol Resistance',
@@ -50,37 +51,169 @@ const skillCombatProwess = [{
     percent: 99
 }];
 
+
+// Img data
+
+const imgData = [{
+    id: 'img0',
+    imgSrc: 'https://blendswap.com/static/blendImages/2017/07/31/Blend/89075/a02482c6582a31ffeab16def39877cf2.jpg',
+    imgTitle: 'Portal Gun',
+    imgDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, laudantium nihil dignissimos amet, quos repellat voluptatum dolores dolor veritatis dolorum sit, ducimus doloribus quidem assumenda consectetur. Amet at atque nam dicta magnam quaerat reiciendis expedita nihil, rerum reprehenderit temporibus minima maxime. Eius, ex? Animi, voluptatem.'
+ },
+ {
+    id: 'img1',
+    imgSrc: 'https://i.pinimg.com/originals/a9/2f/71/a92f7159fdf1e794a9421ec2c3a10223.jpg',
+    imgTitle: 'Meeseeks Box',
+    imgDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, laudantium nihil dignissimos amet, quos repellat voluptatum dolores dolor veritatis dolorum sit, ducimus doloribus quidem assumenda consectetur. Amet at atque nam dicta magnam quaerat reiciendis expedita nihil, rerum reprehenderit temporibus minima maxime. Eius, ex? Animi, voluptatem.'
+  },
+  {
+    id: 'img2',
+    imgSrc: 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/4c4bb6103450627.5f4d99902007b.png',
+    imgTitle: 'Grappling Shoes',
+    imgDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, laudantium nihil dignissimos amet, quos repellat voluptatum dolores dolor veritatis dolorum sit, ducimus doloribus quidem assumenda consectetur. Amet at atque nam dicta magnam quaerat reiciendis expedita nihil, rerum reprehenderit temporibus minima maxime. Eius, ex? Animi, voluptatem.'
+  },
+  {
+    id: 'img3',
+    imgSrc: 'https://preview.redd.it/9vjgg3sxq5e71.png?width=960&crop=smart&auto=webp&v=enabled&s=2e4e1eb77ff175e4ee6920db5fe3848cc2a4ecd0',
+    imgTitle: 'Laser Gun',
+    imgDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, laudantium nihil dignissimos amet, quos repellat voluptatum dolores dolor veritatis dolorum sit, ducimus doloribus quidem assumenda consectetur. Amet at atque nam dicta magnam quaerat reiciendis expedita nihil, rerum reprehenderit temporibus minima maxime. Eius, ex? Animi, voluptatem.'
+  },
+  {
+    id: 'img4',
+    imgSrc: 'https://p4.wallpaperbetter.com/wallpaper/29/673/216/rick-and-morty-rick-sanchez-morty-smith-wallpaper-preview.jpg',
+    imgTitle: 'Space Ship',
+    imgDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, laudantium nihil dignissimos amet, quos repellat voluptatum dolores dolor veritatis dolorum sit, ducimus doloribus quidem assumenda consectetur. Amet at atque nam dicta magnam quaerat reiciendis expedita nihil, rerum reprehenderit temporibus minima maxime. Eius, ex? Animi, voluptatem.'
+  },
+  {
+    id: 'img5',
+    imgSrc: 'https://m.media-amazon.com/images/M/MV5BNWZjMTQ5ZjMtMTYyOC00OWIzLTk0OGItZTVkZGY0OTgwZTQ1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg',
+    imgTitle: 'Interdimentional Cable',
+    imgDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, laudantium nihil dignissimos amet, quos repellat voluptatum dolores dolor veritatis dolorum sit, ducimus doloribus quidem assumenda consectetur. Amet at atque nam dicta magnam quaerat reiciendis expedita nihil, rerum reprehenderit temporibus minima maxime. Eius, ex? Animi, voluptatem.'
+  },
+  {
+    id: 'img6',
+    imgSrc: 'https://fab-user.s3.ap-northeast-2.amazonaws.com/avatar/e8aef356ca5106110da60e409d1b84e4/6C2851F9DD2E54F159E4374AAA8C6FE8.png',
+    imgTitle: 'Butter Robo',
+    imgDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, laudantium nihil dignissimos amet, quos repellat voluptatum dolores dolor veritatis dolorum sit, ducimus doloribus quidem assumenda consectetur. Amet at atque nam dicta magnam quaerat reiciendis expedita nihil, rerum reprehenderit temporibus minima maxime. Eius, ex? Animi, voluptatem.'
+  },
+  {
+    id: 'img7',
+    imgSrc: 'https://i.pinimg.com/564x/fe/c3/3b/fec33b66f10028bcd415a1a8aaee0aa0.jpg',
+    imgTitle: 'Dog Helmet',
+    imgDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, laudantium nihil dignissimos amet, quos repellat voluptatum dolores dolor veritatis dolorum sit, ducimus doloribus quidem assumenda consectetur. Amet at atque nam dicta magnam quaerat reiciendis expedita nihil, rerum reprehenderit temporibus minima maxime. Eius, ex? Animi, voluptatem.'
+  },
+  {
+    id: 'img8',
+    imgSrc: 'https://d6ehjqrqtzoun.cloudfront.net/a888d75e-99ff-42b5-9e82-e75ed033ad85.jpeg',
+    imgTitle: 'Clon Machine',
+    imgDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, laudantium nihil dignissimos amet, quos repellat voluptatum dolores dolor veritatis dolorum sit, ducimus doloribus quidem assumenda consectetur. Amet at atque nam dicta magnam quaerat reiciendis expedita nihil, rerum reprehenderit temporibus minima maxime. Eius, ex? Animi, voluptatem.'
+  },]
+
 // Intellect list
 const ulIntellect = document.getElementById('ulIntellect');
 // Physical Fitness list
 const ulPhysicalFit = document.getElementById('ulPhysicalFit');
 // Combat Prowess list
 const ulCombatPro = document.getElementById('ulCombatPro');
+// image gallery
+const imgGallery = document.getElementById('gallery');
+// modal window
+const modalWindow = document.getElementById('modalWindow');
+const modalWindowImg  = document.getElementById('modalWindowImg');
+const modalWindowText = document.getElementById('modalWindowText');
+const modalWindowTitle = document.getElementById('modalWindowTitle');
+const modalWindowCloseMark = document.getElementById('modalWindowCloseMark');
+
 
 const docFragment = document.createDocumentFragment();
-
-// function to make dinamics skill list
+const docFragment2 = document.createDocumentFragment();
+// procedure to make dinamics skill list
 const dinamicUnorderedList = (skillList, listId)=>{
     skillList.forEach(value =>{
         
         const li = document.createElement('li');
         const div = document.createElement('div'); //we use this div to make the skill bar
         const percentContainer = document.createElement('div');
+        const container = document.createElement('div');
         const span = document.createElement('span');
     
         div.className= 'skillBar';
         div.style.width = `${value.percent}%`;
         percentContainer.textContent=`${value.percent}%`;
         span.textContent = value.skillName;
-        span.appendChild(percentContainer);
-        li.appendChild(span);
+        container.appendChild(span);
+        container.appendChild(percentContainer);
+        li.appendChild(container);
         li.appendChild(div);
         docFragment.appendChild(li);
     })
-    console.log(docFragment)
     listId.appendChild(docFragment)
 }
 
+// procedure to make a dinamic gallery
+const dinamicImgGalery = (imgList,listId) =>{
+    imgList.forEach((imgData,i) =>{
+
+        const div = document.createElement('div');
+        const h2 =  document.createElement('h2');
+        const img =  document.createElement('img');
+    
+        div.className = 'gallery__item';
+        img.className = 'gallery__item__img';
+    
+        h2.textContent = imgData.imgTitle;
+        img.setAttribute('id',`img${i}` )
+        img.setAttribute('src', `${imgData.imgSrc}`);
+        img.setAttribute('alt', `${imgData.imgTitle}`);
+        div.appendChild(img);
+        div.appendChild(h2);
+        docFragment2.appendChild(div)  ;  
+    
+    })
+    listId.appendChild(docFragment2)
+}
+
+// procedure to show the modal-window changing adding a class
+const modalWindowToggle = ()=>{
+    modalWindow.classList.toggle('portfolio__modal-window-active')
+}
+
+// function to return the data of the selected image
+const imageData = (imgList,imgSelected,imgData)=>{
+    let index;
+    imgList.forEach((data,i) =>{
+        if(data.id === imgSelected){
+           index = i;
+        }
+    })
+  
+    return imgData[index];
+}
+
+modalWindowCloseMark.addEventListener('click',()=>{modalWindowToggle()});
+
+
+
+// skills 
 dinamicUnorderedList(skillIntellect, ulIntellect);
 dinamicUnorderedList(skillPhysicalFitness, ulPhysicalFit);
 dinamicUnorderedList(skillCombatProwess, ulCombatPro);
+
+// img 
+
+dinamicImgGalery(imgData,imgGallery);
+const images = document.querySelectorAll('.gallery__item__img')
+
+
+images.forEach(img => {
+    img.addEventListener('click',()=>{
+    
+        let data = imageData(images, img.id,imgData);
+        modalWindowImg.setAttribute('src', `${data.imgSrc}`);  
+        modalWindowImg.setAttribute('alt', `${data.imgTitle}`);  
+        modalWindowText.textContent = data.imgDescription;
+        modalWindowTitle.textContent = data.imgTitle;
+        modalWindowToggle();
+    })
+})
+  
